9e75e19723983660d738d4b468819cef68e4e3cd
Aviskar KC <aviskarkc10@gmail.com>
2018-09-26 17:35:21 +0545

    Add documentation for the props to be sent to the component

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,3 +1,5 @@
+import './styles.scss';
+
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
@@ -9,32 +11,108 @@ const DONUT_CHART_DATA = [
   { name: 'Drool over hooman\'s food', count: 30, color: '#DA5B41' },
   { name: 'Play with hooman', count: 40, color: '#9AA4AF' }
 ];
-const codeBlock = { backgroundColor: 'black', color: 'white', height: '170px', width: '300px', padding: '10px', float: 'left', marginLeft: '20px' };
-const chartContainer = { float: 'left', display: 'inline-block' };
-const exampleContainer = { padding: '20px', height: '230px', float: 'left' };
-const link = { textDecoration: 'none' };
 
 class Demo extends Component {
   render() {
-    return <div>
-      <h1><a style={link} href="https://twitter.com/dog_feelings">Thoughts of dogs.</a></h1>
-      <div style={exampleContainer}>
-        <h2>1. Basic Example</h2>
-        <div></div>
-        <div style={chartContainer}>
-          <DonutChart
-            innerRadius={90}
-            outerRadius={100}
-            transition={true}
-            displayTooltip={true}
-            strokeWidth={3}
-            svgClass="example1"
-            pieClass="pie1"
-            data={DONUT_CHART_DATA} />
+    return (
+      <div>
+        <h1><a className="link" href="https://twitter.com/dog_feelings">Thoughts of dog.</a></h1>
+        <div>
+          <h2>Import the component</h2>
+          <div className="code-block">{`import DonutChart from 'react-d3-donut';`}</div>
         </div>
-        <div style={codeBlock}>
-          <pre>
-            {`<DonutChart
+        <div>
+          <h2>Props used to display chart.</h2>
+          <table>
+            <thead>
+              <tr>
+                <td>Name</td>
+                <td>Description</td>
+                <td>Type</td>
+                <td>Required</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>data</td>
+                <td>The data to be displayed on the chart.</td>
+                <td>array</td>
+                <td>true</td>
+              </tr>
+              <tr>
+                <td>outerRadius</td>
+                <td>The radius of the bigger circle.</td>
+                <td>number</td>
+                <td>true</td>
+              </tr>
+              <tr>
+                <td>innerRadius</td>
+                <td>The radius of the smaller circle.</td>
+                <td>number</td>
+                <td>true</td>
+              </tr>
+              <tr>
+                <td>strokeWidth</td>
+                <td>Set the gap between each pie.</td>
+                <td>number</td>
+                <td>true</td>
+              </tr>
+              <tr>
+                <td>svgClass</td>
+                <td>Set the class name of the svg to be appended to the DOM.</td>
+                <td>string</td>
+                <td>true</td>
+              </tr>
+              <tr>
+                <td>pieClass</td>
+                <td>Set the class name of the pie chart to be appended to the DOM.</td>
+                <td>string</td>
+                <td>true</td>
+              </tr>
+              <tr>
+                <td>transition</td>
+                <td>Set whether or not you want the animation on your chart.</td>
+                <td>boolean</td>
+                <td>false</td>
+              </tr>
+              <tr>
+                <td>displayTooltip</td>
+                <td>Set whether or not you want tooltip on your chart.</td>
+                <td>boolean</td>
+                <td>false</td>
+              </tr>
+            </tbody>
+          </table>
+          <div>
+            <h3>Note: data should have the following shape</h3>
+            <div className="code-block">
+              <pre>
+                {`data = [{
+  count: 20       // Value of the property. Required.
+  color: #000000  // Color code for the pie's color. Required.
+  name: 'My name' // Optional value. Used to display in the tooltip.
+ }]`}
+              </pre>
+            </div>
+          </div>
+        </div>
+        <div className="example-container">
+          <h2>1. Basic Example</h2>
+          <div></div>
+          <div className="chart-container">
+            <DonutChart
+              innerRadius={90}
+              outerRadius={100}
+              transition={true}
+              displayTooltip={true}
+              strokeWidth={3}
+              svgClass="example1"
+              pieClass="pie1"
+              data={DONUT_CHART_DATA} />
+          </div>
+          <div className="code-block example-block">
+            <pre>
+              {`<DonutChart
    innerRadius={90}
    outerRadius={100}
    transition={true}
@@ -43,26 +121,26 @@ class Demo extends Component {
    displayTooltip={true}
    strokeWidth={3}
    data={data} />`}
-          </pre>
+            </pre>
+          </div>
         </div>
-      </div>
-      <div style={exampleContainer}>
-        <h2>2. Without transition</h2>
-        <div></div>
-        <div style={chartContainer}>
-          <DonutChart
-            innerRadius={90}
-            outerRadius={100}
-            transition={false}
-            displayTooltip={true}
-            strokeWidth={3}
-            svgClass="example2"
-            pieClass="pie2"
-            data={DONUT_CHART_DATA} />
-        </div>
-        <div style={codeBlock}>
-          <pre>
-            {`<DonutChart
+        <div className="example-container">
+          <h2>2. Without transition</h2>
+          <div></div>
+          <div className="chart-container">
+            <DonutChart
+              innerRadius={90}
+              outerRadius={100}
+              transition={false}
+              displayTooltip={true}
+              strokeWidth={3}
+              svgClass="example2"
+              pieClass="pie2"
+              data={DONUT_CHART_DATA} />
+          </div>
+          <div className="code-block example-block">
+            <pre>
+              {`<DonutChart
    innerRadius={90}
    outerRadius={100}
    transition={false}
@@ -71,26 +149,26 @@ class Demo extends Component {
    displayTooltip={true}
    strokeWidth={3}
    data={data} />`}
-          </pre>
+            </pre>
+          </div>
         </div>
-      </div>
-      <div style={exampleContainer}>
-        <h2>3. Without stroke width</h2>
-        <div></div>
-        <div style={chartContainer}>
-          <DonutChart
-            innerRadius={90}
-            outerRadius={100}
-            transition={true}
-            displayTooltip={true}
-            strokeWidth={0}
-            svgClass="example3"
-            pieClass="pie3"
-            data={DONUT_CHART_DATA} />
-        </div>
-        <div style={codeBlock}>
-          <pre>
-            {`<DonutChart
+        <div className="example-container">
+          <h2>3. Without stroke width</h2>
+          <div></div>
+          <div className="chart-container">
+            <DonutChart
+              innerRadius={90}
+              outerRadius={100}
+              transition={true}
+              displayTooltip={true}
+              strokeWidth={0}
+              svgClass="example3"
+              pieClass="pie3"
+              data={DONUT_CHART_DATA} />
+          </div>
+          <div className="code-block example-block">
+            <pre>
+              {`<DonutChart
    innerRadius={90}
    outerRadius={100}
    transition={true}
@@ -99,26 +177,26 @@ class Demo extends Component {
    displayTooltip={true}
    strokeWidth={0}
    data={data} />`}
-          </pre>
+            </pre>
+          </div>
         </div>
-      </div>
-      <div style={ exampleContainer }>
-        <h2>4. A pie chart</h2>
-        <div></div>
-        <div style={chartContainer}>
-          <DonutChart
-            innerRadius={0}
-            outerRadius={100}
-            transition={true}
-            displayTooltip={true}
-            strokeWidth={3}
-            svgClass="example4"
-            pieClass="pie4"
-            data={DONUT_CHART_DATA} />
-        </div>
-        <div style={codeBlock}>
-          <pre>
-            {`<DonutChart
+        <div className="example-container">
+          <h2>4. A pie chart</h2>
+          <div></div>
+          <div className="chart-container">
+            <DonutChart
+              innerRadius={0}
+              outerRadius={100}
+              transition={true}
+              displayTooltip={true}
+              strokeWidth={3}
+              svgClass="example4"
+              pieClass="pie4"
+              data={DONUT_CHART_DATA} />
+          </div>
+          <div className="code-block example-block">
+            <pre>
+              {`<DonutChart
    innerRadius={0}
    outerRadius={100}
    transition={true}
@@ -127,26 +205,26 @@ class Demo extends Component {
    displayTooltip={true}
    strokeWidth={3}
    data={data} />`}
-          </pre>
+            </pre>
+          </div>
         </div>
-      </div>
-      <div style={ exampleContainer }>
-        <h2>5. Without tooltip</h2>
-        <div></div>
-        <div style={chartContainer}>
-          <DonutChart
-            innerRadius={0}
-            outerRadius={100}
-            transition={true}
-            displayTooltip={false}
-            strokeWidth={3}
-            svgClass="example5"
-            pieClass="pie5"
-            data={DONUT_CHART_DATA} />
-        </div>
-        <div style={codeBlock}>
-          <pre>
-            {`<DonutChart
+        <div className="example-container">
+          <h2>5. Without tooltip</h2>
+          <div></div>
+          <div className="chart-container">
+            <DonutChart
+              innerRadius={0}
+              outerRadius={100}
+              transition={true}
+              displayTooltip={false}
+              strokeWidth={3}
+              svgClass="example5"
+              pieClass="pie5"
+              data={DONUT_CHART_DATA} />
+          </div>
+          <div className="code-block example-block">
+            <pre>
+              {`<DonutChart
    innerRadius={90}
    outerRadius={100}
    transition={true}
@@ -155,26 +233,26 @@ class Demo extends Component {
    displayTooltip={true}
    strokeWidth={3}
    data={data} />`}
-          </pre>
+            </pre>
+          </div>
         </div>
-      </div>
-      <div style={ exampleContainer }>
-        <h2>6. Smaller inner radius</h2>
-        <div></div>
-        <div style={chartContainer}>
-          <DonutChart
-            innerRadius={70}
-            outerRadius={100}
-            transition={true}
-            displayTooltip={true}
-            strokeWidth={3}
-            svgClass="example6"
-            pieClass="pie6"
-            data={DONUT_CHART_DATA} />
-        </div>
-        <div style={codeBlock}>
-          <pre>
-            {`<DonutChart
+        <div className="example-container">
+          <h2>6. Smaller inner radius</h2>
+          <div></div>
+          <div className="chart-container">
+            <DonutChart
+              innerRadius={70}
+              outerRadius={100}
+              transition={true}
+              displayTooltip={true}
+              strokeWidth={3}
+              svgClass="example6"
+              pieClass="pie6"
+              data={DONUT_CHART_DATA} />
+          </div>
+          <div className="code-block example-block">
+            <pre>
+              {`<DonutChart
    innerRadius={70}
    outerRadius={100}
    transition={true}
@@ -183,10 +261,11 @@ class Demo extends Component {
    displayTooltip={true}
    strokeWidth={3}
    data={data} />`}
-          </pre>
+            </pre>
+          </div>
         </div>
       </div>
-    </div>;
+    );
   }
 }
 
