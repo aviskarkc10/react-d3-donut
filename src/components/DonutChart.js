00f0cdb71aa54f1f2d9e9c48e82711b58e287a4e
Aviskar KC <aviskarkc10@gmail.com>
2018-10-10 14:28:42 +0545

    Use a better name for tooltip class

--- src/components/DonutChart.js
+++ src/components/DonutChart.js
@@ -200,7 +200,7 @@ class DonutChart extends Component {
         {(this.props.displayTooltip && Object.keys(this.state.selectedArc).length) ? (
           <DonutChartToolTip
             pieClass={this.props.pieClass}
-            donutClass={this.props.donutClass}
+            tooltipClass={this.props.tooltipClass}
             name={this.state.selectedArc.name || ''}
             count={this.state.selectedArc.count || 0}
             mousePosition={this.state.mousePosition}
@@ -219,7 +219,7 @@ DonutChart.propTypes = {
   svgClass: string.isRequired,
   pieClass: string.isRequired,
   transition: bool,
-  donutClass: string,
+  tooltipClass: string,
   displayTooltip: bool
 };
 
--- src/components/DonutChartTooltip.js
+++ src/components/DonutChartTooltip.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { number, shape, string } from 'prop-types';
 
 function DonutChartTooltip(props) {
-  const { count, name, mousePosition, pieClass, donutClass } = props;
+  const { count, name, mousePosition, pieClass, tooltipClass } = props;
   const donutChart = document.getElementsByClassName(pieClass);
   const container = donutChart.length && donutChart[0].getBoundingClientRect();
   const positionY = mousePosition.y - container.height;
   const positionX = mousePosition.x;
   const studentCount = `${name}: ${count}`;
-  const donutStyle = donutClass || 'donut-tooltip'; 
+  const donutStyle = tooltipClass || 'donut-tooltip'; 
 
   return (
     <div className={donutStyle} style={{ marginLeft: positionX, marginTop: positionY }}>
@@ -20,7 +20,7 @@ function DonutChartTooltip(props) {
 DonutChartTooltip.propTypes = {
   count: number,
   name: string,
-  donutClass: string,
+  tooltipClass: string,
   mousePosition: shape({
     x: number,
     y: number
